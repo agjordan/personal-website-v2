ea004b02796c8c9bda68541b1926be5102b58d9a
agjordan <alex.jordan@clim8invest.com>
2024-09-23 14:57:37 +0100

    sections setup

--- src/components/scroll-content.tsx
+++ src/components/scroll-content.tsx
@@ -1,125 +1,146 @@
 export function ScrollContent() {
   return (
     <div className="flex flex-1 flex-col gap-10">
-      <p id="about" className="flex flex-col gap-4">
-        <span>
-          I'm a London-based software engineer with a background in product
-          management and a passion for creating and delivering user-centric
-          products. I discovered my love for coding while working as a product
-          manager, where I found myself enjoying the process of building Python
-          scripts to streamline workflows.
-        </span>
-        <span>
-          Initially, I thought data engineering would be my path—after working
-          on several ETL projects and teaching myself SQL—but I soon realized
-          that my product management experience aligned well with front-end
-          development, especially in creating intuitive user experiences.
-        </span>
-        <span>
-          A few years later, I've worked across the full stack and feel most
-          confident coding in TypeScript. I also have solid experience in Python
-          and Java, with side projects that gave me a chance to dabble in C,
-          Lua, and Ruby. I'm driven by big goals and thrive in fast-paced
-          environments where I can take on interesting challenges, make a quick
-          impact, and continue growing.
-        </span>
+      <About />
+      <Experience />
+      <Projects />
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div id="about" className="flex flex-col md:pt-10">
+      <SectionTitle title="About" />
+      <p>
+        I'm a London-based software engineer with a background in product
+        management and a passion for creating and delivering user-centric
+        products. I discovered my love for coding while working as a product
+        manager, where I found myself enjoying the process of building Python
+        scripts to streamline workflows.
       </p>
-      <p id="experience">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea
-        voluptates culpa modi ratione iste accusamus sequi, eveniet blanditiis
-        suscipit odit consectetur quibusdam laboriosam maxime eum molestiae
-        ipsum repellat. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolor ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
-        blanditiis suscipit odit consectetur quibusdam laboriosam maxime eum
-        molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste
-        accusamus sequi, eveniet blanditiis suscipit odit consectetur quibusdam
-        laboriosam maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Dolor ab ea voluptates culpa modi
-        ratione iste accusamus sequi, eveniet blanditiis suscipit odit
-        consectetur quibusdam laboriosam maxime eum molestiae ipsum
-        repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-        ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
-        blanditiis suscipit odit consectetur quibusdam laboriosam maxime eum
-        molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste
-        accusamus sequi, eveniet blanditiis suscipit odit consectetur quibusdam
-        laboriosam maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Dolor ab ea voluptates culpa modi
-        ratione iste accusamus sequi, eveniet blanditiis suscipit odit
-        consectetur quibusdam laboriosam maxime eum molestiae ipsum
-        repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-        ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
-        blanditiis suscipit odit consectetur quibusdam laboriosam maxime eum
-        molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste
-        accusamus sequi, eveniet blanditiis suscipit odit consectetur quibusdam
-        laboriosam maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Dolor ab ea voluptates culpa modi
-        ratione iste accusamus sequi, eveniet blanditiis suscipit odit
-        consectetur quibusdam laboriosam maxime eum molestiae ipsum
-        repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-        ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
-        blanditiis suscipit odit consectetur quibusdam laboriosam maxime eum
-        molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste
-        accusamus sequi, eveniet blanditiis suscipit odit consectetur quibusdam
-        laboriosam maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Dolor ab ea voluptates culpa modi
-        ratione iste accusamus sequi, eveniet blanditiis suscipit odit
-        consectetur quibusdam laboriosam maxime eum molestiae ipsum
-        repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-        ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
+      <p>
+        Initially, I thought data engineering would be my path—after working on
+        several ETL projects and teaching myself SQL—but I soon realized that my
+        product management experience aligned well with front-end development,
+        especially in creating intuitive user experiences.
       </p>
-      <p id="projects">
-        Spent the last 2 years building all the things at HAI. But here are some
-        older projects from when I was learning JS/React <br /> Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
-        culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit
-        odit consectetur quibusdam laboriosam maxime eum molestiae ipsum
-        repellat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-        ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
-        blanditiis suscipit odit consectetur quibusdam laboriosam maxime eum
-        molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste
-        accusamus sequi, eveniet blanditiis suscipit odit consectetur quibusdam
-        laboriosam maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Dolor ab ea voluptates culpa modi
-        ratione iste accusamus sequi, eveniet blanditiis suscipit odit
-        consectetur quibusdam laboriosam maxime eum molestiae ipsum
-        repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-        ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
-        blanditiis suscipit odit consectetur quibusdam laboriosam maxime eum
-        molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste
-        accusamus sequi, eveniet blanditiis suscipit odit consectetur quibusdam
-        laboriosam maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Dolor ab ea voluptates culpa modi
-        ratione iste accusamus sequi, eveniet blanditiis suscipit odit
-        consectetur quibusdam laboriosam maxime eum molestiae ipsum
-        repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-        ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
-        blanditiis suscipit odit consectetur quibusdam laboriosam maxime eum
-        molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste
-        accusamus sequi, eveniet blanditiis suscipit odit consectetur quibusdam
-        laboriosam maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Dolor ab ea voluptates culpa modi
-        ratione iste accusamus sequi, eveniet blanditiis suscipit odit
-        consectetur quibusdam laboriosam maxime eum molestiae ipsum
-        repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-        ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
-        blanditiis suscipit odit consectetur quibusdam laboriosam maxime eum
-        molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste
-        accusamus sequi, eveniet blanditiis suscipit odit consectetur quibusdam
-        laboriosam maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Dolor ab ea voluptates culpa modi
-        ratione iste accusamus sequi, eveniet blanditiis suscipit odit
-        consectetur quibusdam laboriosam maxime eum molestiae ipsum
-        repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-        ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
+      <p>
+        A few years later, I've worked across the full stack and feel most
+        confident coding in TypeScript. I also have solid experience in Python
+        and Java, with side projects that gave me a chance to dabble in C, Lua,
+        and Ruby. I'm driven by big goals and thrive in fast-paced environments
+        where I can take on interesting challenges, make a quick impact, and
+        continue growing.
       </p>
     </div>
   );
+}
+
+function Experience() {
+  return (
+    <div id="experience">
+      <SectionTitle title="Experience" />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea
+      voluptates culpa modi ratione iste accusamus sequi, eveniet blanditiis
+      suscipit odit consectetur quibusdam laboriosam maxime eum molestiae ipsum
+      repellat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+      ab ea voluptates culpa modi ratione iste accusamus sequi, eveniet
+      blanditiis suscipit odit consectetur quibusdam laboriosam maxime eum
+      molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet blanditiis suscipit odit consectetur quibusdam laboriosam
+      maxime eum molestiae ipsum repellat.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor ab ea voluptates culpa modi ratione iste accusamus
+      sequi, eveniet
+    </div>
+  );
+}
+
+function Projects() {
+  return (
+    <div id="projects">
+      <SectionTitle title="About" />
+      Spent the last 2 years building all the things at HAI. But here are some
+      older projects from when I was learning JS/React <br /> Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Dolor ab ea voluptates culpa modi
+      ratione iste accusamus sequi, eveniet blanditiis suscipit odit consectetur
+      quibusdam laboriosam maxime eum molestiae ipsum repellat. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates culpa
+      modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet blanditiis suscipit odit
+      consectetur quibusdam laboriosam maxime eum molestiae ipsum repellat.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor ab ea voluptates
+      culpa modi ratione iste accusamus sequi, eveniet
+    </div>
+  );
+}
+
+type SectionTitleProps = {
+  title: string;
+};
+function SectionTitle({ title }: SectionTitleProps) {
+  return <h3 className="text-lg underline md:hidden">{title}</h3>;
 }
